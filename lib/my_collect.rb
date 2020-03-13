d284bf928ce9fa25f7809750b7868c9649e8466e
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-13 05:06:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,9 @@ def my_collect(arr)
     i = 0
     new_arr = []
     while i < arr.length
-      yield arr[i]
-        new_arr << i
-        i += 1
-    
+      new_arr << yield(arr[i])
+      i += 1
+
     end
     return new_arr
     
